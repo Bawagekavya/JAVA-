5641dbc31309c42431a3406300cb4ab000def04d
Bawagekavya <kavyabawage.xworkz@gmail.com>
2024-12-24 08:25:43 +0530

    Jdbc

--- jdbc-app/src/main/java/com/xwork/jdbc/InstituteRunner.java
+++ jdbc-app/src/main/java/com/xwork/jdbc/InstituteRunner.java
@@ -6,6 +6,7 @@ public class InstituteRunner {
 
     public static void main(String[] args) {
 
+        Connection connection = null;;
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             System.out.println("Driver loaded succesfully");
@@ -13,10 +14,21 @@ public class InstituteRunner {
             String url = "jdbc:mysql://localhost:3306/Institute";
             String username = "root";
             String password = "7259958546";
-            DriverManager.getConnection(url, username, password);
+            connection = DriverManager.getConnection(url, username, password);
             System.out.println("Established connection successfully");
+            Statement statement = connection.createStatement();
+            String insert = " insert into institute_info values (0,'Educadd','Kengeri')";
+            int row = statement.executeUpdate(insert);
+            System.out.println("rows affected :"+ row);
         } catch (ClassNotFoundException | SQLException classNotFoundException) {
             System.out.println("JDBC driver not found :" + classNotFoundException.getMessage());
+        }finally {
+            try {
+                connection.close();
+            }catch (SQLException e)
+            {
+                System.out.println(e.getMessage());
+            }
         }
     }
 }
